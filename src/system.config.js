fb127110a27f0d150b06573399df52f41ce16e1b
Parakh Jain <parja@microsoft.com>
2017-06-26 16:59:55 -0700

    Fixing main link

--- src/system.config.js
+++ src/system.config.js
@@ -32,7 +32,7 @@
     // packages tells the System loader how to load when no filename and/or no extension
     packages: {
       app: {
-        main: './main.js',
+        main: './dist/main.js',
         defaultExtension: 'js',
         meta: {
           './*.js': {
