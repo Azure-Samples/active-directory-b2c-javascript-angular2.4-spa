98fb013828a6047350bff500d17345625ac979d3
Parakh Jain <parja@microsoft.com>
2017-06-28 14:59:39 -0700

    Updating system.config

--- src/system.config.js
+++ src/system.config.js
@@ -32,7 +32,7 @@
     // packages tells the System loader how to load when no filename and/or no extension
     packages: {
       app: {
-        main: './dist/main.js',
+        main: './main.js',
         defaultExtension: 'js',
         meta: {
           './*.js': {
