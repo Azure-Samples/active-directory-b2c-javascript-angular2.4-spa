c3c217691eac682344ecda35fcf6375bffcf365e
Jean-Marc Prieur <jmprieur@microsoft.com>
2020-03-31 17:58:01 -0700

    Archiving the sample

--- src/app/components/home.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component }    from '@angular/core';
-import { Location }     from '@angular/common';
-
-@Component({
-  templateUrl: './home.html'
-})
-
-export class HomeComponent {constructor(){}}
